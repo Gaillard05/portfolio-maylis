28f30018ff81bde1bf17947b6e6a03081b844118
Gaillard05 <maylisgaillard@hotmail.com>
2026-03-10 16:25:58 +0100

    modification de la connexion du site en local avec dev.env au lieu de .env dans le fichier autoload et ajout de formatage email pour corriger les problemes affichage des accents

--- app/services/mailer.php
+++ app/services/mailer.php
@@ -1,5 +1,5 @@
 <?php
-
+// Mailer.php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -8,25 +8,45 @@ class Mailer {
     private $mailer;
 
     public function __construct() {
-        $this->mailer = new PHPMailer();
+        $this->mailer = new PHPMailer(true);
 
+        // --- Configuration SMTP MailTrap ---
         $this->mailer->isSMTP();
-        $this->mailer->Host = $_ENV['HOST'] ?? null;;
-        $this->mailer->SMTPAuth = $_ENV['SMTPAUTH'] ?? null;
-        $this->mailer->Username = $_ENV['USERNAME'] ?? null;
-        $this->mailer->Password = $_ENV['PASSWORD'] ?? null;
+        $this->mailer->Host = $_ENV['HOST'] ?? '';
+        $this->mailer->SMTPAuth = true;
+        $this->mailer->Username = $_ENV['USERNAME'] ?? ''; 
+        $this->mailer->Password = $_ENV['PASSWORD'] ?? '';  
+        $this->mailer->SMTPSecure = $_ENV['SMTPSECURE'] ?? '';     
         $this->mailer->Port = $_ENV['PORT'] ?? null;
+        $this->mailer->CharSet = 'UTF-8';
+        $this->mailer->Encoding = 'base64';
+
     }
 
-    public function sendContactEmail($nom, $mail, $message, $object) {
+    /**
+     * Envoie un email depuis le formulaire de contact
+     * @param string $nom - nom du visiteur
+     * @param string $emailVisiteur - email du visiteur
+     * @param string $message - message du visiteur
+     * @param string $objet - objet du mail
+     * @return bool
+     */
+    public function sendContactEmail($nom, $mail, $message, $objet) {
         try {
             
             $this->mailer->isHTML(true);
-            $this->mailer->setFrom('mg.contact.digital@gmail.com', 'maylis gaillard');
-            $this->mailer->addAddress($mail);
-            $this->mailer->Subject = $object;
-            $this->mailer->Body =
-                "$message";
+
+            // --- Expéditeur obligatoire : ton domaine validé ---
+            $this->mailer->setFrom('contact@maylis-gaillard.fr', 'maylis-gaillard');
+
+            // --- Reply-To : permet de répondre directement au visiteur ---
+            $this->mailer->addReplyTo($mail, $nom);
+
+            // --- Destinataire réel  ---
+            $this->mailer->addAddress('mg.contact.digital@gmail.com');
+
+            $this->mailer->Subject = $objet;
+            $this->mailer->Body = "$message";
 
             return $this->mailer->send();
 
